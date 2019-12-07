c982c8bc35146b6c5edb0f45d7c21b219e2be1f8
victor-myha <victormyhaletsky@gmail.com>
2019-12-07 15:01:06 +0200

    lesson4

--- auth/index.js
+++ auth/index.js
@@ -1,20 +1,30 @@
 const { Router } = require("express");
 const router = Router();
-const { addUser } = require("../fs");
+const { addUser,removeUser } = require("../fs");
+const fs = require("fs");
 
 router.post("/register", (req, res) => {
-  // console.log(req.body);
   let data = req.body;
   data.id = Date.now();
   console.log(data);
-  addUser(req.body);
-  res.redirect("/");
+  let db = JSON.parse(fs.readFileSync("db/database.json", "utf-8"));
+  let fill = db.users.filter(item => item.tel === data.tel || item.email === data.email);
+  if (fill.length == 0){
+    addUser(data);
+    res.redirect('/');
+  }
+  else{
+    res.redirect('/register');
+  }
 });
 
-router.delete("/user", (req, res) => {
-  console.log(req.body);
-  addUser(req.body);
-  res.redirect("/");
+router.delete("/remove_user/:id", (req, res) => {
+  removeUser(req.params.id, data => {
+    if (data == 200){
+        res.sendStatus(200);
+    }
+  });
+
 });
 
 module.exports = router;
--- fs/index.js
+++ fs/index.js
@@ -1,13 +1,7 @@
 const fs = require("fs");
 
 module.exports.addUser = el => {
-  // fs.writeFileSync("hello.txt", "hello world");
-  // fs.appendFileSync("hello.txt", "hello ");
   let db = JSON.parse(fs.readFileSync("db/database.json", "utf-8"));
-  // let a = {
-  //   firstname: "Sofa",
-  //   tel: "911"
-  // };
   db.users.push(el);
   fs.writeFileSync("db/database.json", JSON.stringify(db));
 };
@@ -21,7 +15,21 @@ module.exports.addUser = el => {
 // console.log('++++++',a)
 
 
-// console.log(db);
-module.exports.removeUser = id => {
-  console.log('id', id)
+module.exports.removeUser = (id ,callback) => {
+  let db = JSON.parse(fs.readFileSync("db/database.json", "utf-8"));
+  console.log(db.users);
+  db.users.forEach(item => {
+    if(item.id == id){
+      let current = db.users.indexOf(item)
+      db.users.splice(current,1)
+    }
+  });
+  fs.writeFileSync("db/database.json", JSON.stringify(db));
+  callback(200)
 }
+
+// var a = ['aa','bb','cc','dd']
+// a.indexOf('cc')
+// var current = a.indexOf('cc')
+// a.splice(current,1)
+// ["cc"]
